f0bf79276918e904eea56730b554d24135a1bcb8
Rahul kumar Agrahari <66835286+BrooCode@users.noreply.github.com>
2022-02-19 15:30:04 +0530

    Update download.py

--- schology-backend/plag-checker/download.py
+++ schology-backend/plag-checker/download.py
@@ -2,13 +2,12 @@ import requests
 import os
 
 
-def download(arr):
+def download(arr, assignmentID):
     for key in arr:
         student_id = key
         url = arr[key]
-        r = requests.get(url) 
+        r = requests.get(url)
         temp = str(student_id) + ".txt"
-        path = os.path.join("asssigment",temp)
+        path = os.path.join(assignmentID,temp)
         with open(path,'wb') as f:
             f.write(r.content)
-
